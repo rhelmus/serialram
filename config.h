e5ec5c63ca05503391ebe10eaed021c8ec007d3d
Rick Helmus <rhelmus@gmail.com>
2015-05-10 15:32:29 +0200

    .

--- config.h
+++ config.h
@@ -2,11 +2,12 @@
 #define SERRAM_CONFIG_H
 
 // Use SPIFIFO on Teensy 3.X boards
-//#define SERIALRAM_USESPIFIFO
+#define SERIALRAM_USESPIFIFO
 
 // If using SPIFIFO, do not reverse byte order to speed up reads.
 // Enabling this might be useful if you plan to use the RAM with other
 // code
-//#define SERIALRAM_USENODATASWAP
+// UNDONE: doesn't work with swapping
+#define SERIALRAM_USENODATASWAP
 
 #endif // SERRAM_CONFIG_H
